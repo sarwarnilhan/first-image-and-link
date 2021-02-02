329eb76398652e1c57d3681085e69ddd6219d8de
nilhan sarwar <77848392+sarwarnilhan@users.noreply.github.com>
2021-02-03 01:38:48 +0600

    Add files via upload

---
+++ image.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>IMAGE AND LINK</title>
+	</head>
+	<body>
+
+<h2 align="center">IMAGE SHOWING</h2>
+
+
+<div align="center">
+<img src= "images/mountain.jpg" alt="mountain" height="200" width="300">
+<a href="https://www.pexels.com/royalty-free-images/">CLICK HERE</a>
+
+</div>
+<pre align = "center">
+An <strong>image</strong> is a representation of the external form of a person or thing in art.
+The general impression that a person, organization, or product presents to the public.
+ An image can be described in terms of vector graphics or raster graphics.
+  An image stored in raster form is sometimes called a bitmap.
+
+
+</pre>
+
+
+
+
+
+
+
+</body>
+
+</html>
